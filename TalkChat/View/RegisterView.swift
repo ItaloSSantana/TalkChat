cdff09433a6c2e990c1ea150c9e61e94731d36df
ItaloSSantana <110575556+ItaloSSantana@users.noreply.github.com>
2022-10-31 13:08:26 -0700

    variaveis tela 2

--- TalkChat/View/RegisterView.swift
+++ TalkChat/View/RegisterView.swift
@@ -5,4 +5,60 @@
 //  Created by Italo Santana on 28/10/22.
 //
 
-import Foundation
+import UIKit
+
+class RegisterView: UIView {
+    
+    let createLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Crie sua conta!"
+        return label
+    }()
+    
+    let userView: UIView = {
+       let uView = UIView()
+        return uView
+    }()
+    
+    let userTextField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+    
+    let emailView: UIView = {
+        let eView = UIView()
+        return eView
+    }()
+    
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+    
+    let passwordView: UIView = {
+       let pView = UIView()
+        return pView
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        return textField
+    }()
+    
+    let registerButton: UIButton = {
+       let button = UIButton()
+        return button
+    }()
+
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+    
+    
+}
