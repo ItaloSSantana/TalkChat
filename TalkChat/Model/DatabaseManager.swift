d759b1b9a44d6a2161c66b4cb321a02f07123993
ItaloSSantana <110575556+ItaloSSantana@users.noreply.github.com>
2022-12-03 10:47:11 -0800

    New register auth correct

--- TalkChat/Model/DatabaseManager.swift
+++ TalkChat/Model/DatabaseManager.swift
@@ -20,19 +20,19 @@ final class DatabaseManager {
         
         public func userExists(with email: String,
                                completion: @escaping ((Bool) -> Void)){
-            database.child(email).observeSingleEvent(of: .value) { snapshot in
+            var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+            safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+            database.child(safeEmail).observeSingleEvent(of: .value) { snapshot in
                 guard snapshot.value as? String != nil else {
                     completion(false)
                     return
                 }
                 completion(true)
             }
-            
         }
-        
         /// Inserts new user to database
     public func insertUser(user: ChatAppUser) {
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "userName": user.userName
         ])
     }
@@ -42,4 +42,9 @@ final class DatabaseManager {
 struct ChatAppUser {
     let userName: String
     let emailAddress: String
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
 }
